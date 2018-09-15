1e3284f31f8782967e56f5c03ac8fb73a33a075c
Brett Allred <brettallred@gmail.com>
2018-09-15 10:08:39 -0600

    fixed wished products to allow you to specify wishlist

--- app/controllers/spree/wished_products_controller.rb
+++ app/controllers/spree/wished_products_controller.rb
@@ -3,17 +3,15 @@ class Spree::WishedProductsController < Spree::StoreController
 
   def create
     @wished_product = Spree::WishedProduct.new(wished_product_attributes)
-    @wishlist = spree_current_user.wishlist
-
-    if @wishlist.include? params[:wished_product][:variant_id]
-      @wished_product = @wishlist.wished_products.detect { |wp| wp.variant_id == params[:wished_product][:variant_id].to_i }
+    @wished_product.wishlist ||= spree_current_user.wishlist
+    if @wished_product.wishlist.include? params[:wished_product][:variant_id]
+      @wished_product = @wished_product.wishlist.wished_products.detect { |wp| wp.variant_id == params[:wished_product][:variant_id].to_i }
     else
-      @wished_product.wishlist = spree_current_user.wishlist
       @wished_product.save
     end
 
     respond_with(@wished_product) do |format|
-      format.html { redirect_to wishlist_url(@wishlist) }
+      format.html { redirect_to wishlist_url(@wished_product.wishlist ) }
     end
   end
 
